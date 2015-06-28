552304144e1bd2db2721c69754172a9ca33e510e
Andreas <stag_o_saurus@hotmail.com>
2015-06-28 20:45:31 +0200

    Only if PlayerCamSync.cs is active: player position is set to camera position

--- Projekt/Rift Wingsuit - Server/Assets/PlayerCamSync.cs
+++ Projekt/Rift Wingsuit - Server/Assets/PlayerCamSync.cs
@@ -12,10 +12,13 @@ public class PlayerCamSync : MonoBehaviour {
 
 	// Offet 
 	private Vector3 offsetPos;
+	private Vector3 oldPlayerPos;
 	
 	void Start () {
 		// Get rift cam by tag
 		cam = GameObject.FindGameObjectWithTag ("MainCamera");
+		oldPlayerPos = new Vector3 (19110, 3836, 7570);
+
 
 		// Set offset
 		offsetPos = new Vector3 (5.0f, -2.0f, 0.0f);
@@ -23,7 +26,8 @@ public class PlayerCamSync : MonoBehaviour {
 
 	void Update () {
 		// Sync playerpos 
-		//player.GetComponent<Transform> ().position += cam.GetComponent<Transform> ().position + offsetPos;
+		player.transform.position = oldPlayerPos;
+		player.transform.position = cam.transform.position;// + offsetPos;
 
 	}
 }
