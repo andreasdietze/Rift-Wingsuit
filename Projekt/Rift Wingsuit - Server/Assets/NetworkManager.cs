414ee72eb4e8eb268322e7dc5a7a2a50e14dc82f
Andreas <stag_o_saurus@hotmail.com>
2015-06-30 13:35:44 +0200

    Small update for connecting to server via keypress (space) and project settings

--- Projekt/Rift Wingsuit - Server/Assets/NetworkManager.cs
+++ Projekt/Rift Wingsuit - Server/Assets/NetworkManager.cs
@@ -56,7 +56,7 @@ public class NetworkManager : MonoBehaviour
             hostList = MasterServer.PollHostList();
         }
 
-		if (Input.GetKey(KeyCode.Space))
+		if (Input.GetKeyDown(KeyCode.Space))
 			StartServer();
 
     }
