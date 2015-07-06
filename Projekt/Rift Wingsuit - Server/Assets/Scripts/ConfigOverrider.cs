5ba666a336e3d6fcbb051ee4b13d4c3cf358568a
DerBasti <seb.nickel@googlemail.com>
2015-07-06 23:06:10 +0200

    Newest update

---
+++ Projekt/Rift Wingsuit - Server/Assets/Scripts/ConfigOverrider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConfigOverrider : MonoBehaviour {
+
+    public long detailDistance = 3000;
+    public bool overrideStartPos = false;
+    public Vector3 startPosition = new Vector3(5000, 5000, 5000);
+
+	// Use this for initialization
+	void Start () {
+        Terrain.activeTerrain.basemapDistance = detailDistance;
+        if (overrideStartPos)
+        {
+            Camera.main.transform.position = startPosition;
+        }
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        Terrain.activeTerrain.basemapDistance = detailDistance;
+	}
+}
