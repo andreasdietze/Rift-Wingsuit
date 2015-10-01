d9721705daa71c48f07d0df49221a7288280938d
DerBasti <seb.nickel@googlemail.com>
2015-10-01 23:11:06 +0200

    Changed: .asset of world packed in order to avoid file size problem with github Added: various assets for the world.

--- Projekt/Rift Wingsuit - Server/Assets/Scripts/ConfigOverrider.cs
+++ Projekt/Rift Wingsuit - Server/Assets/Scripts/ConfigOverrider.cs
@@ -4,14 +4,18 @@ using System.Collections;
 public class ConfigOverrider : MonoBehaviour {
 
     public long detailDistance = 3000;
+    private float detailObjectDistance;
     public bool overrideStartPos = false;
     public Vector3 startPosition = new Vector3(5000, 5000, 5000);
+
+    private Vector3 forestPosition = new Vector3(26500, 1, 16100);
 
 	// Use this for initialization
     void Start()
     {
+        detailObjectDistance = detailDistance / 2;
         Terrain.activeTerrain.basemapDistance = detailDistance;
-        Terrain.activeTerrain.detailObjectDistance = detailDistance / 4;
+        Terrain.activeTerrain.detailObjectDistance = detailObjectDistance;
 
         if (overrideStartPos)
         {
@@ -23,5 +27,13 @@ public class ConfigOverrider : MonoBehaviour {
     void Update()
     {
         Terrain.activeTerrain.basemapDistance = detailDistance;
+        float distX = Camera.main.transform.position.x - forestPosition.x;
+        float distY = Camera.main.transform.position.z - forestPosition.z;
+
+        double dist = Mathf.Sqrt(distX * distX + distY * distY);
+        if (dist <= (detailObjectDistance / 4))
+        {
+            Terrain.activeTerrain.detailObjectDistance = detailObjectDistance*2;
+        }
 	}
 }
